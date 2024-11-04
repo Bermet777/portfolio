1469f82bd3fdcb6c535b5e15890344080785d601
bermet <bermet.alibek@gmail.com>
2024-11-03 23:56:49 -0500

    tools new

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -27,7 +27,7 @@ const Technologies = () => {
           <div>
             <strong className="block text-xl mb-3">Tools</strong>
             <p className="text-gray-700">
-              Git, Docker, GitHub, Jira, Bash, Asana
+              Git, Docker, GitHub, Jira, Bash
             </p>
           </div>
         </div>
